78752304bc6bf3cc22150b893f08a595dd8aaec8
Pete Schwamb <pete@schwamb.net>
2022-02-04 17:35:17 -0600

    Show rudimentary pod state info

--- src/App.js
+++ src/App.js
@@ -1,23 +1,38 @@
 import logo from './logo.svg';
 import './App.css';
+import { connect, sendMsg } from "./api";
+import React, { useEffect, useState } from 'react'
 
 function App() {
+
+  const [podState, setPodState] = useState({});
+
+  useEffect(() => {
+    connect((msg) => {
+      setPodState(msg)
+      console.log("New pod state:", msg)
+    });
+  }, [])
+
+  function send() {
+    console.log("hello");
+    sendMsg("hello");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="Header">
+        <span>Pod Simulator</span>
+      </div>
+
+      <div className="podstate">
+        <div><span>LTK</span> <span>{podState.LKT}</span></div>
+        <div><span>EapAkaSeq</span> <span>{podState.EapAkaSeq}</span></div>
+        <div><span>Id</span> <span>{podState.Id}</span></div>
+        <div><span>MsgSeq</span> <span>{podState.MsgSeq}</span></div>
+        <div><span>CmdSeq</span> <span>{podState.CmdSeq}</span></div>
+      </div>
+      <button onClick={send}>Send Message</button>
     </div>
   );
 }
---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+// api/index.js
+var socket = new WebSocket("ws://rpi:8080/ws");
+
+let connect = cb => {
+  console.log("Attempting Connection...");
+
+  socket.onopen = () => {
+    console.log("Successfully Connected");
+  };
+
+  socket.onmessage = msg => {
+    cb(JSON.parse(msg.data));
+  };
+
+  socket.onclose = event => {
+    console.log("Socket Closed Connection: ", event);
+  };
+
+  socket.onerror = error => {
+    console.log("Socket Error: ", error);
+  };
+};
+
+let sendMsg = msg => {
+  console.log("sending msg: ", msg);
+  socket.send(msg);
+};
+
+export { connect, sendMsg };
